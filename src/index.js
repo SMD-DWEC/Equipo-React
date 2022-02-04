dc37a766abb1bdb2d69d96a11f70a25412acdeca
2DAW04 <2DAW04@esvirgua.es>
2022-02-04 12:38:27 +0100

    Lista resultados versión inicial

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+/**
+ * Parte 2:
+ * Al pulsar el botón se generará una Lista de Resultados
+ * (que será un componente React), que mostrará entre 3 y 5 resultados (aleatorios)
+ * (que también serán componentes React).
+ * Cada resultado será un div a modo de tarjeta con un título y un texto generados al azar.
+ */
 
-class Controlador extends React.Component {
+//Resultados se muestra tras darle a enviar en el formulario.
+class Resultados extends React.Component {
+  constructor(props) {
+    super(props);
+    this.estado = {
+        resultados: ["Hola", "Adiós", "Esto es otro resultado"]
+    };
+  }
+
+  //Renderizamos los resultados...
   render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {
-          this.props.value
-        }
-      </button>
+    return(
+      <Tarjeta 
+        resultado = {this.estado.resultados}
+      />
     );
   }
 }
+
+/**
+ * Clase que genera las tarjetas
+ * La invocamos con un render en Resultados, con lo que le llega a su constructor una prop (resultado)
+ * como en nuestra clase tarjeta no tenemos constructor llama al super y coge el prop directamente.
+ */
+class Tarjeta extends React.Component {
+
+  /**
+   * Renderizamos un div.
+   * @returns El div con el resultado del prop
+   */
+  render() {
+    return this.props.resultado.map((result, Symbol) => {
+        console.log(result);
+        return ( //Identificamos a cada div con un id único.
+          <div key={Symbol}>
+            {result}
+          </div>
+        );
+    });
+  }
+}
+
+
 // ========================================
 
-ReactDOM.render(<Controlador />, document.getElementById("root"));
+ReactDOM.render(<Resultados />, document.getElementById("root"));
