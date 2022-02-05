e30902cdf03a21571a15abe17c6775ec1dbe9bad
kiliann <wilian_kilian@hotmail.com>
2022-02-05 22:30:08 +0100

    menu acabado

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,25 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import {headerwrapper } from "./styles/Header"
 
-function Header(){
-    const [open, setOpen] = useStat(false)
 
-    const handleClick = () => {
-        setOpen(!open);
-    };
+function Opciones(props){
+    return(
+        <li>Opcion {props.num}</li>
+    )
+}
+
+function Menu(){
+    let item =[]
+
+    for(let i = 0; i<3; i++){
+        items.push(<li key={i}><a href={'#'}>Opcion {i +1}</a> </li>)
+    }
+
+    return(
+
+
+        <ul>
+            {item}
+        </ul>
+    )
 }
